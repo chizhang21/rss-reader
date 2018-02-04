d61e0f152ecf4ee3bd2bcb505cd06083b83dd733
cashzhang21 <cz21ok@gmail.com>
2018-02-04 23:44:31 +0800

    [ashley][init]init main view.

---
+++ app/src/main/java/com/cashzhang/ashley/MainFragment.java
@@ -0,0 +1,32 @@
+package com.cashzhang.ashley;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
+import uk.co.senab.actionbarpulltorefresh.library.Options;
+import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
+
+/**
+ * Created by hadoop on 02/02/2018.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        PullToRefreshLayout layout = (PullToRefreshLayout) inflater.inflate(R.layout.feed_list, container, false);
+
+        return layout;
+    }
+}
